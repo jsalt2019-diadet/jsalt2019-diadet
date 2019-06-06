83dcf45efb5a3b4fbb192f28091104706da4ca00
Jesus Villalba <jesus.antonio.villalba@gmail.com>
2019-06-06 12:07:55 -0400

    some run files were renamed

--- egs/jsalt2019-diadet/v1/steps_be/eval-be-diar-v1.py
+++ egs/jsalt2019-diadet/v1/steps_be/eval-be-diar-v1.py
@@ -19,29 +19,79 @@ import logging
 import numpy as np
 
 from hyperion.hyp_defs import float_cpu, config_logger
-from hyperion.utils.scp_list import SCPList
+#from hyperion.utils.scp_list import SCPList
 from hyperion.utils.trial_ndx import TrialNdx
 from hyperion.utils.trial_scores import TrialScores
-from hyperion.helpers import TrialDataReader as TDR
+from hyperion.helpers import MultiTestTrialDataReader as TDR
 from hyperion.helpers import PLDAFactory as F
 from hyperion.transforms import TransformList
 
 
-def combine_diar_scores(ndx, diar_ndx, diar2orig, diar_scores):
+# def combine_diar_scores(ndx, diar_ndx, diar2orig, diar_scores):
 
-    d2o = SCPList.load(diar2orig, sep=' ')
-    d2o = d2o.filter(diar_ndx.seg_set)
+#     d2o = SCPList.load(diar2orig, sep=' ')
+#     d2o = d2o.filter(diar_ndx.seg_set)
+#     scores = np.zeros(ndx.trial_mask.shape, dtype=float_cpu())
+#     for j in xrange(len(ndx.seg_set)):
+#         idx = d2o.file_path == ndx.seg_set[j]
+#         diar_scores_j = diar_scores[:, idx]
+#         scores_j = np.max(diar_scores_j, axis=1)
+#         scores[:,j] = scores_j
+
+#     return scores
+
+
+# def eval_plda(iv_file, ndx_file, diar_ndx_file, enroll_file, diar2orig,
+#               preproc_file,
+#               model_file, score_file, plda_type,
+#               **kwargs):
+    
+#     logging.info('loading data')
+#     if preproc_file is not None:
+#         preproc = TransformList.load(preproc_file)
+#     else:
+#         preproc = None
+
+#     tdr = TDR(iv_file, diar_ndx_file, enroll_file, None, preproc)
+#     x_e, x_t, enroll, diar_ndx = tdr.read()
+
+#     logging.info('loading plda model: %s' % (model_file))
+#     model = F.load_plda(plda_type, model_file)
+    
+#     t1 = time.time()
+    
+#     logging.info('computing llr')
+#     scores = model.llr_1vs1(x_e, x_t)
+    
+#     dt = time.time() - t1
+#     num_trials = len(enroll) * x_t.shape[0]
+#     logging.info('scoring elapsed time: %.2f s. elapsed time per trial: %.2f ms.'
+#           % (dt, dt/num_trials*1000))
+
+#     logging.info('combine cluster scores') 
+#     ndx = TrialNdx.load(ndx_file)
+#     scores = combine_diar_scores(ndx, diar_ndx, diar2orig, scores)
+
+#     logging.info('saving scores to %s' % (score_file))
+#     s = TrialScores(enroll, ndx.seg_set, scores)
+#     s = s.align_with_ndx(ndx)
+#     s.save_txt(score_file)
+
+
+def combine_diar_scores(ndx, orig_seg, subseg_scores):
+
     scores = np.zeros(ndx.trial_mask.shape, dtype=float_cpu())
     for j in xrange(len(ndx.seg_set)):
-        idx = d2o.file_path == ndx.seg_set[j]
-        diar_scores_j = diar_scores[:, idx]
-        scores_j = np.max(diar_scores_j, axis=1)
+        idx = orig_seg == ndx.seg_set[j]
+        subseg_scores_j = subseg_scores[:, idx]
+        scores_j = np.max(subseg_scores_j, axis=1)
         scores[:,j] = scores_j
 
     return scores
 
 
-def eval_plda(iv_file, ndx_file, diar_ndx_file, enroll_file, diar2orig,
+
+def eval_plda(iv_file, ndx_file, diar_ndx_file, enroll_file, test_subseg2orig_file,
               preproc_file,
               model_file, score_file, plda_type,
               **kwargs):
@@ -52,8 +102,8 @@ def eval_plda(iv_file, ndx_file, diar_ndx_file, enroll_file, diar2orig,
     else:
         preproc = None
 
-    tdr = TDR(iv_file, diar_ndx_file, enroll_file, None, preproc)
-    x_e, x_t, enroll, diar_ndx = tdr.read()
+    tdr = TDR(iv_file, ndx_file, enroll_file, None, test_subseg2orig, preproc)
+    x_e, x_t, enroll, ndx, orig_seg = tdr.read()
 
     logging.info('loading plda model: %s' % (model_file))
     model = F.load_plda(plda_type, model_file)
@@ -69,8 +119,7 @@ def eval_plda(iv_file, ndx_file, diar_ndx_file, enroll_file, diar2orig,
           % (dt, dt/num_trials*1000))
 
     logging.info('combine cluster scores') 
-    ndx = TrialNdx.load(ndx_file)
-    scores = combine_diar_scores(ndx, diar_ndx, diar2orig, scores)
+    scores = combine_diar_scores(ndx, orig_seg, scores)
 
     logging.info('saving scores to %s' % (score_file))
     s = TrialScores(enroll, ndx.seg_set, scores)
@@ -87,9 +136,10 @@ if __name__ == "__main__":
 
     parser.add_argument('--iv-file', dest='iv_file', required=True)
     parser.add_argument('--ndx-file', dest='ndx_file', required=True)
-    parser.add_argument('--diar-ndx-file', dest='diar_ndx_file', required=True)
+    #parser.add_argument('--diar-ndx-file', dest='diar_ndx_file', required=True)
     parser.add_argument('--enroll-file', dest='enroll_file', required=True)
-    parser.add_argument('--diar2orig', dest='diar2orig', required=True)
+    #parser.add_argument('--diar2orig', dest='diar2orig', required=True)
+    parser.add_argument('--test-subseg2orig-fie', dest='test_subseg2orig_file', required=True)
     parser.add_argument('--preproc-file', dest='preproc_file', default=None)
 
     TDR.add_argparse_args(parser)
