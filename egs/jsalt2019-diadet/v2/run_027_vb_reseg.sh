80939ab60ef57aa0cc4f85991b4ef37bf8b6397c
Latane Bullock <1thisisfornothing1@gmail.com>
2019-07-01 20:56:42 -0400

    run 27 toy test

--- egs/jsalt2019-diadet/v2/run_027_vb_reseg.sh
+++ egs/jsalt2019-diadet/v2/run_027_vb_reseg.sh
@@ -21,6 +21,12 @@ config_file=default_config.sh
 . parse_options.sh || exit 1;
 . $config_file
 
+
+be_dir=exp/be_diar/$nnet_name/$be_diar_name
+score_dir=exp/diarization/$nnet_name/$be_diar_name
+ami_dev_Mix="jsalt19_spkdiar_ami_dev_Mix-Headset"
+
+
 # num_components=1024 # the number of UBM components (used for VB resegmentation)
 num_components=128 # the number of UBM components (used for VB resegmentation)
 # ivector_dim=400 # the dimension of i-vector (used for VB resegmentation)
@@ -30,7 +36,7 @@ ivector_dim=100 # the dimension of i-vector (used for VB resegmentation)
 if [ $stage -le 1 ]; then
   output_rttm_dir=exp/VB/rttm
   mkdir -p $output_rttm_dir || exit 1;
-  exp/diarization/2a.1.voxceleb_div2/lda120_plda_voxceleb/jsalt19_spkdiar_ami_dev_Mix-Headset/plda_scores_tbest/rttm \ 
+  cat exp/diarization//plda_scores_tbest/rttm \ 
     > $output_rttm_dir/pre_VB_rttm
   init_rttm_file=$output_rttm_dir/pre_VB_rttm
 
@@ -39,12 +45,13 @@ if [ $stage -le 1 ]; then
   # initize the VB system. The following script uses kaldi_io. 
   # You could use `sh ../../../tools/extras/install_kaldi_io.sh` to install it
   VB/diarization/VB_resegmentation.sh --nj 20 --cmd "$train_cmd --mem 10G" \
-    --initialize 1 data/jsalt19_spkdiar_ami_dev_Mix-Headset $init_rttm_file exp/VB \
+    --initialize 1 data/$ami_dev_Mix $init_rttm_file exp/VB \
     exp/VB/diag_ubm_ami_train_$num_components/final.dubm exp/extractor_diag_ami_train_c${num_components}_i${ivector_dim}/final.ie || exit 1; 
 
   # Compute the DER after VB resegmentation
   mkdir -p exp/VB/results || exit 1;
-  md-eval.pl -1 -c 0.25 -r data/callhome/fullref.rttm -s $output_rttm_dir/VB_rttm 2> exp/VB/log/VB_DER.log \
+  md-eval.pl -1 -c 0.25 -r data/$nnet_name/$be_diar_name/$ami_dev_Mix/diarization.rttm \
+    -s $output_rttm_dir/VB_rttm 2> exp/VB/log/VB_DER.log \
     > exp/VB/results/VB_DER.txt
   der=$(grep -oP 'DIARIZATION\ ERROR\ =\ \K[0-9]+([.][0-9]+)?' \
     exp/VB/results/VB_DER.txt)
