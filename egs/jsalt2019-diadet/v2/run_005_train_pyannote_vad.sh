cab6d34f245af4e87e7f68c006d225c064af7544
Latane Bullock <1thisisfornothing1@gmail.com>
2019-07-11 16:44:27 -0400

    SRI after CHIME5 wait mechanism updated

--- egs/jsalt2019-diadet/v2/run_005_train_pyannote_vad.sh
+++ egs/jsalt2019-diadet/v2/run_005_train_pyannote_vad.sh
@@ -12,27 +12,28 @@
 set -e
 
 
-for PROTOCOL in AMI.SpeakerDiarization.MixHeadset \
-                # AMI.SpeakerDiarization.Array1 \
-                # AMI.SpeakerDiarization.Array2 \
-                # BabyTrain.SpeakerDiarization.All \
-                # CHiME5.SpeakerDiarization.U01 \
-                # CHiME5.SpeakerDiarization.U06
-do
-  $pyannote_cmd --gpu 1 exp/vad/${PROTOCOL}/train.log \
-      steps_vad/train.sh ${PROTOCOL} &
+# for PROTOCOL in AMI.SpeakerDiarization.MixHeadset \
+#                 AMI.SpeakerDiarization.Array1 \
+#                 AMI.SpeakerDiarization.Array2 \
+#                 BabyTrain.SpeakerDiarization.All \
+#                 CHiME5.SpeakerDiarization.U01 
+# do
 
-done
+#   $pyannote_cmd --gpu 1 exp/vad/${PROTOCOL}/train.log \
+#       steps_vad/train.sh ${PROTOCOL} &
+
+# done
+
+
+
+# PROTOCOL=CHiME5.SpeakerDiarization.U06
+# $pyannote_cmd --gpu 1 exp/vad/${PROTOCOL}/train.log \
+#     steps_vad/train.sh ${PROTOCOL} 
+
+
+PROTOCOL=SRI.SpeakerDiarization.All
+$pyannote_cmd --gpu 1 exp/vad/${PROTOCOL}/train.log steps_vad/train.sh ${PROTOCOL}
+
 
 # FIXME: what does this wait do?
 wait
-
-# # wait for CHiME5 training to be completed (at least 200 epochs) before submitting tuning on SRI
-# FAKE_PROTOCOL_DIR=CHiME5.SpeakerDiarization.U06
-# FAKE_EXPERIMENT_DIR="exp/vad/${FAKE_PROTOCOL}"
-# MODEL_PT=${FAKE_EXPERIMENT_DIR}/models/train/${FAKE_PROTOCOL}.train/weights/0200.pt
-# wait_file ${MODEL_PT}
-
-# PROTOCOL=SRI.SpeakerDiarization.All
-# $pyannote_cmd --gpu 1 exp/vad/${PROTOCOL}/train.log steps_vad/train.sh ${PROTOCOL}
-
