5a07c94ea8edbd474b8b9f04ac3017aa26d1c174
Latane Bullock <1thisisfornothing1@gmail.com>
2019-07-26 09:56:47 -0400

    VB overlap reseg more fully functional

--- egs/jsalt2019-diadet/v4/local/diar_ovl_assignment.py
+++ egs/jsalt2019-diadet/v4/local/diar_ovl_assignment.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 #
+#               
 #
 # Apache 2.0
 #
-# 2019 Latané Bullock (JSALT 2019) 
-# Taken heavily from Zili Huang's (Johns Hopkins University) VB_resegmentation.py 
+# 
+#  -- Modified from Zili Huang's (Johns Hopkins University) VB_resegmentation.py 
+#  -- Uses the speaker attribution (q) matrix from VB resegmentation and an overlap 
+#  -- hypothesis rttm to assign speakers to overlapped frames 
+#
+# JSALT 2019, Latané Bullock
 
 import numpy as np
-# from pyannote.core.utils import numpy as pyanp
-# import pyannote.database as db
-# from pyannote.core import segment
 import argparse
 
 
@@ -96,9 +98,6 @@ def create_rttm_output(uttname, pri_sec, predicted_label, output_dir, channel):
 def main():
     parser = argparse.ArgumentParser(description='Frame-level overlap reassignment with speaker posterior attributions')
     parser.add_argument('ovl_dir', type=str, help='Path to directory where we have necessary files for overlap reassignment')
-    # parser.add_argument('ovl_rttm', type=str, help='Path to an rttm specifying with regions have overlap')
-    # parser.add_argument('vad_rttm', type=str, help='Path to an rttm specifying regions of speech/non-speech')
-    # parser.add_argument('output_dir', type=str, help='Path where output rttm will be saved')
 
     args = parser.parse_args()
     print(args)
@@ -110,33 +109,29 @@ def main():
         n_frames = utt2num_frames[utt]
 
         vad = rttm2one_hot(utt, utt2num_frames, '{}/rttm_in'.format(args.ovl_dir))
-        unique, counts = np.unique(vad, return_counts=True)
-        voiced_frames = dict(zip(unique, counts))[1]
+        # unique, counts = np.unique(vad, return_counts=True)
+        # voiced_frames = dict(zip(unique, counts))[1]
 
         overlap = rttm2one_hot(utt, utt2num_frames, '{}/overlap.rttm'.format(args.ovl_dir))
-        # overlap = overlap * vad
-        unique, counts = np.unique(overlap, return_counts=True)
-        print('Overlap counts: ', dict(zip(unique, counts)))
-        print(overlap.shape)
-        print()
        
         # Keep only the voiced frames (0 denotes the silence 
         # frames, 1 denotes the overlapping speech frames).
         mask = (vad >= 1)
 
-        # Reminder: q is only for voiced frames
+        # Remember: q is only for voiced frames
         q_out = np.load('{}/q_mats/{}_q_out.npy'.format(args.ovl_dir, utt))
         
-        # predicted_label_voiced = np.argmax(q_out, 1) + 2
+        # Standard procedure from VB reseg - take the most likely speaker 
         predicted_label_voiced = np.argsort(-q_out, 1)[:,0] + 2 
         predicted_label = (np.zeros(len(mask))).astype(int)
         predicted_label[mask] = predicted_label_voiced
-        create_rttm_output(utt, 'pri', predicted_label, args.ovl_dir, 1)
+        # This is the 'primary' speaker for each frame
+        create_rttm_output(utt, 'pri', predicted_label, args.ovl_dir, channel=1)
 
+        # Write "secondary" speakers for overlap regions 
+        # -- take second most likely speaker for each overlap frame 
         predicted_label_voiced = np.argsort(-q_out, 1)[:,1] + 2 
         predicted_label = (np.zeros(len(mask))).astype(int)
-        # predicted_label[mask] = predicted_label_voiced
-        # predicted_label = predicted_label * overlap
 
         frame_t_voiced = 0
         for t in range(len(mask)):
@@ -145,61 +140,9 @@ def main():
                     predicted_label[t] = predicted_label_voiced[frame_t_voiced]
                 frame_t_voiced += 1
             
-        create_rttm_output(utt, 'sec', predicted_label, args.ovl_dir, 1)
+        create_rttm_output(utt, 'sec', predicted_label, args.ovl_dir, channel=1)
 
 
-        # q = q.T
-        # q_argsort = np.argsort(-q, axis=0)[:2,:]
-
-        # print(q.shape)
-        # print(q[:, :10])
-        # print(q_argsort[:, :10])
-        
-        unique, counts = np.unique(vad, return_counts=True)
-        print('VAD counts: ', dict(zip(unique, counts)))
-        print(vad.shape)
-
-
-        
-        # print('Number voiced frames: {}'.format(np.count_nonzero(vad)))
-        # Ensure agreement in number of frames 
-        # assert q.shape[1] == voiced_frames
-        
-        # # Our frames are 25ms
-        # sw = segment.SlidingWindow(0.025, 0.025)
-
-        # # Load matrices - overlap and vad from pyannote, q directly form numpy 
-        # # overlap = pyanp.one_hot_encoding(db.util.load_rttm(args.ovl_rttm), sw)
-        # protocol = db.get_protocol('AMI.SpeakerDiarization.MixHeadset')
-        # print()
-        # for test_file in protocol.test():
-        #     print(type(test_file))
-        #     ref = test_file['annotation']
-        #     uem = test_file['annotated']
-        # print(type(ref))
-        # print(type(uem))
-        # print(type(db.util.load_rttm(args.vad_rttm)))
-        # vad = pyanp.one_hot_encoding(ref, uem, sw)
-        # print(vad[0].data.shape)
-        
-        
-
-        # active_spk = np.zeros((2, n_frames), dtype=int)
-        # print('{} : {}'.format('active_spk', active_spk.shape))
-
-        # frame_t_voiced = 0
-        # for frame_t in range(n_frames):
-        #     if not vad[frame_t]:
-        #         continue
-        #     active_spk[0, frame_t] = q_argsort[0, frame_t_voiced]
-        #     # if overlap[frame_t]:
-        #     #     active_spk[1, frame_t] = q_argsort[1, frame_t_voiced]
-        #     frame_t_voiced += 1
-        
-        # create_rttm_output(utt, 'pri', active_spk[0,:], args.ovl_dir, 1)
-        # # create_rttm_output(utt, 'sec', active_spk[1,:], args.ovl_dir, 1)
-
-        # # assert True == False
     return 0
 
 
--- egs/jsalt2019-diadet/v4/run_028_diar_overlap_assignment.sh
+++ egs/jsalt2019-diadet/v4/run_028_diar_overlap_assignment.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # Copyright
-#                2019   Latané Bullock (JSALT 2019)
+#            
 #
 # Apache 2.0.
 #
+#
+#
+# JSALT 2019, Latané Bullock
+
 
 
 . ./cmd.sh
@@ -18,24 +22,28 @@ config_file=default_config.sh
 
 
 ovl_out_dir=exp/overlap_models/test_raw
-vad_out_dir=exp/vad
-vb_out_dir=
+vb_suff=VB
 ovl_suff=OVLassign
 
 score_dir=exp/diarization/$nnet_name/$be_diar_name
 
-# retrieves all dsets which have had VB resegmentation performed
+# Retrieves all dsets which we want to perform resegmentation on
+# We first get all dsets which would have a corresponding VB reseg
+# Can perform filtering with inverse grep if needed
 dsets_path=$score_dir
-dsets=`find $dsets_path  -maxdepth 1 -name "jsalt19_spkdiar*VB" | xargs -l basename | sort`
+dsets=`find $dsets_path  -maxdepth 1 -name "jsalt19_spkdiar*" \
+  | xargs -l basename \
+  | sort \
+  | grep -v VB \
+  | grep -v sri \
+  `
 
-# TESTING(FIXME)
-dsets="jsalt19_spkdiar_ami_dev_Mix-Headset_VB"
+
 
 if [ $stage -le 1 ]; then
-	# Given an overlap RTTM, VAD rttm, and the VB-HMM-GMM q matrix (speaker poster attributions), 
-	# we perform frame-level reassignment to two speakers in overlap segments
 
 	for dset in $dsets; do
+		
 		# We need to do some name parsing
 		if [[ "$dset" =~ .*ami.* ]];then
       		corp=ami
@@ -57,48 +65,56 @@ if [ $stage -le 1 ]; then
 
 		# TESTING(FIXME) need to add the rest of the mics
 		if [[ "$dset" =~ .*Array1.* ]];then
-      		mic=Array1-01
+      		mic='_Array1-01'
     	elif [[ "$dset" =~ .*Array2.* ]];then
-      		mic=Array2-01
+      		mic='_Array2-01'
     	elif [[ "$dset" =~ .*Mix-Headset.* ]];then
-      		mic=Mix-Headset
-		else echo "dset -- $dset -- unable to be parsed"; exit 1; 
+      		mic='_Mix-Headset'
+    	elif [[ "$dset" =~ .*U01.* ]];then
+      		mic='_U01'
+    	elif [[ "$dset" =~ .*U06.* ]];then
+      		mic='_U06'
+		else mic='';
+		fi
+
+		dset_vb=${dset}_${vb_suff}
+		if [ ! -d ${dsets_path}/${dset_vb} ]; then 
+			continue
 		fi
 		
-		
 		# This is the dir that the overlap assignment will use 
-		output_dir=${dsets_path}/${dset}_${ovl_suff}
+		output_dir=${dsets_path}/${dset_vb}_${ovl_suff}
 		
 		mkdir -p $output_dir
 
-		#TESTING(FXIME)
+		#TESTING(FXIME)-clear dir before starting
 		rm -rf $output_dir/tmp
 		rm -rf $output_dir/*.log
 		rm -rf $output_dir/result.*
 		rm -rf $output_dir/rttm
 
 		# get rttm from BEFORE VB reseg
-		# cp $dsets_path/$dset/rttm $output_dir/rttm_in
+		cp $dsets_path/$dset/plda_scores_tbest/rttm $output_dir/rttm_in
 		
-		overlap_rttm=${ovl_out_dir}/diar_overlap_${corp}_${part}_${mic}.rttm
+		overlap_rttm=${ovl_out_dir}/diar_overlap_${corp}_${part}${mic}.rttm
 		cat $overlap_rttm > $output_dir/overlap.rttm
 		
-		# The data dir should contain a pyannoteVAD, utt2spk, and utt2num_frames which are of interest
-		data_dir=data/jsalt19_spkdiar_${corp}_${part}_${mic}
+		# The data dir should contain a utt2spk and utt2num_frames used in VB 
+		data_dir=data/jsalt19_spkdiar_${corp}_${part}${mic}
 		cp $data_dir/utt2spk $output_dir/
 		cp $data_dir/utt2num_frames $output_dir/
 		
 		# We need the q matrix for each utterance from VB resegmentation
 		mkdir -p $output_dir/q_mats
 		mkdir -p $output_dir/tmp
-		# cp $dsets_path/$dset/tmp/*.npy $output_dir/q_mats
+		cp $dsets_path/$dset_vb/tmp/*.npy $output_dir/q_mats
 		
 
 
 		# TESTING(FIXME) - this env activation should be placed more strategiclaly 
 		source activate pyannote
 		
-		# Usage: diar_ovl_assignment.py [-h] q_mat_dir ovl_rttm vad_rttm output_dir
+		# Usage: diar_ovl_assignment.py [-h] <overlap_dir>
 		# At this point, the output dir should contain the q matrix from VB reseg
 		# an overlap rttm, and a vad rttm 
 		python3 local/diar_ovl_assignment.py $output_dir
@@ -117,7 +133,6 @@ ovl_dsets=`find $score_dir  -maxdepth 1 -name "jsalt19_spkdiar*" \
   | grep "$ovl_suff" \
   `
 
-
 if [ $stage -le 2 ]; then
   
   for name in $ovl_dsets
@@ -135,7 +150,7 @@ if [ $stage -le 2 ]; then
     echo $name
 
     # TESTING(FIXME)
-    # we can skip those files which have already been evaluated
+    # we can skip those rttms which have already been evaluated
     if [ -s $score_dir/$name/result.pyannote-der ];then
       continue
     fi
